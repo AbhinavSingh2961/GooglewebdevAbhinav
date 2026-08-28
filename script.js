9176a82395ccc038a8fe07c5efc87daca9df713e
Abhinav Singh <abhu.abhinavsingh@gmail.com>
2026-08-28 12:40:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("gdg web")
+
+let a = "Abhinav"
+const b =18
+
+let shopping= ["bhindi", "kanda", "ayein", "baigan"]
+let num = [1,2,3,4,5]
+
+let obj = { "Abhinav" : 69, "Shreyansh" : 67, "Harshit" : 68}
+
+let grups = {"student_name1":"Abhinav", "student_name2" : "Shreyansh", "student_name3" : "Harshit"}
+   
+
+function greeting(a){
+    console.log("Good Morning", a)
+}
+greeting(a)
+
+console.log(a)
+console.log(b)
+console.log(shopping)
+console.log(num)
+console.log(obj)
+console.log(grups)
